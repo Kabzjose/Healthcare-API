f22668c2e63a65d3301b37bd6c57fcc29a52fdd9
Joseph Ndungu <josekabz00@gmail.com>
2026-07-30 14:20:33 +0300

    fixed jest timeout

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
+    maxWorkers: 1, 
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/server.ts',
@@ -11,4 +12,5 @@ module.exports = {
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   testTimeout: 15000,
+  forceExit: true, 
 };
